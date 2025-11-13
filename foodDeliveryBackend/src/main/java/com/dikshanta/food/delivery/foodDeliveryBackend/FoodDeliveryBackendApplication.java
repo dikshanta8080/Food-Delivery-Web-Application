291324fa8d64974de4a9b78d1a4801428fcc0d13
dikshanta <dikshantaacharya04@gmail.com>
2025-11-13 11:46:57 +0545

    added comments

--- foodDeliveryBackend/src/main/java/com/dikshanta/food/delivery/foodDeliveryBackend/FoodDeliveryBackendApplication.java
+++ foodDeliveryBackend/src/main/java/com/dikshanta/food/delivery/foodDeliveryBackend/FoodDeliveryBackendApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FoodDeliveryBackendApplication {
-
+//main method
 	public static void main(String[] args) {
 		SpringApplication.run(FoodDeliveryBackendApplication.class, args);
 	}
