7055420b47829a76890bae08722c0a51cffeee98
dikshanta <dikshantaacharya04@gmail.com>
2025-11-14 10:33:47 +0545

    added dependencies

---
+++ foodDeliveryBackend/src/main/java/com/dikshanta/food/delivery/foodDeliveryBackend/FoodDeliveryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dikshanta.food.delivery.foodDeliveryBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodDeliveryBackendApplication.class, args);
+	}
+
+}
---
+++ foodDeliveryBackend/src/test/java/com/dikshanta/food/delivery/foodDeliveryBackend/FoodDeliveryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dikshanta.food.delivery.foodDeliveryBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
