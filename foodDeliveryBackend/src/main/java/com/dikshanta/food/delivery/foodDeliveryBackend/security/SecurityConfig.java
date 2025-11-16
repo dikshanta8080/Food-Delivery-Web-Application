9e1134aa43d1ef84d4e179d22f4dd588884bd426
dikshanta <dikshantaacharya04@gmail.com>
2025-11-16 21:35:05 +0545

    added permissions for each endpoint

--- foodDeliveryBackend/src/main/java/com/dikshanta/food/delivery/foodDeliveryBackend/security/SecurityConfig.java
+++ foodDeliveryBackend/src/main/java/com/dikshanta/food/delivery/foodDeliveryBackend/security/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.dikshanta.food.delivery.foodDeliveryBackend.security;
 
 import com.dikshanta.food.delivery.foodDeliveryBackend.services.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -10,24 +12,63 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
 
+import static com.dikshanta.food.delivery.foodDeliveryBackend.enums.Permission.*;
+import static com.dikshanta.food.delivery.foodDeliveryBackend.enums.Role.ADMIN;
+import static com.dikshanta.food.delivery.foodDeliveryBackend.enums.Role.RESTAURANT;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+
+    private static final String adminRoute = "/api/v1/admin/**";
+    private static final String restaurantManagementRoute = "/api/v1/restaurant/**";
+    private static final String publicRoute = "/api/v1/public/**";
+    private final AccessDeniedHandler accessDeniedHandler;
+    private final AuthenticationEntryPoint authenticationEntryPoint;
+
+    @Autowired
+    public SecurityConfig(@Qualifier("customAccessDeniedHandler") AccessDeniedHandler accessDeniedHandler, @Qualifier("customAuthenticationEntryPoint") AuthenticationEntryPoint authenticationEntryPoint) {
+        this.accessDeniedHandler = accessDeniedHandler;
+        this.authenticationEntryPoint = authenticationEntryPoint;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(AbstractHttpConfigurer::disable)
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-                .authorizeHttpRequests(req -> req.anyRequest().authenticated())
+                .authorizeHttpRequests(req -> req
+
+                        .requestMatchers(adminRoute)
+                        .hasRole(ADMIN.name())
+                        .requestMatchers("GET", adminRoute).hasAuthority(ADMIN_READ.getPermissionName())
+                        .requestMatchers("POST", adminRoute).hasAuthority(ADMIN_CREATE.getPermissionName())
+                        .requestMatchers("PUT", adminRoute).hasAuthority(ADMIN_UPDATE.getPermissionName())
+                        .requestMatchers("DELETE", adminRoute).hasAuthority(ADMIN_DELETE.getPermissionName())
+
+                        .requestMatchers(restaurantManagementRoute)
+                        .hasAnyRole(ADMIN.name(), RESTAURANT.name())
+                        .requestMatchers("GET", restaurantManagementRoute).hasAnyAuthority(ADMIN_READ.getPermissionName(), RESTAURANT_READ.getPermissionName())
+                        .requestMatchers("POST", restaurantManagementRoute).hasAnyAuthority(ADMIN_CREATE.getPermissionName(), RESTAURANT_CREATE.getPermissionName())
+                        .requestMatchers("PUT", restaurantManagementRoute).hasAnyAuthority(ADMIN_UPDATE.getPermissionName(), RESTAURANT_UPDATE.getPermissionName())
+                        .requestMatchers("DELETE", restaurantManagementRoute).hasAnyAuthority(ADMIN_DELETE.getPermissionName(), RESTAURANT_DELETE.getPermissionName())
+                        .anyRequest().authenticated())
+
+
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .exceptionHandling(ex -> ex.accessDeniedHandler(accessDeniedHandler).authenticationEntryPoint(authenticationEntryPoint))
                 .build();
     }
 
@@ -36,6 +77,11 @@ public class SecurityConfig {
         DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider(customUserDetailsService);
         authenticationProvider.setPasswordEncoder(passwordEncoder);
         return authenticationProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
